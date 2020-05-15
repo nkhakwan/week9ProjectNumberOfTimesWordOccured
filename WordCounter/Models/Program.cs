ca8a0585b8095570536bf6dfaa0a43ac238ea337
nkhakwan <post_khan@yahoo.com>
2020-05-15 10:24:36 -0700

    Committing after adding empty Program.cs file so that dotnet test start functioning

---
+++ WordCounter/Models/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace WordInASentence
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Hellloooooo");
+      
+
+          
+
+
+
+    }
+  }
+}
