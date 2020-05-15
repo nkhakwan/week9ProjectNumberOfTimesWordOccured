1e347ca2e70f4c46662d79bbd289aed3723f5fa2
nkhakwan <post_khan@yahoo.com>
2020-05-15 10:56:37 -0700

    committing BackEnd.cs with empty functions still to be coded for tests

---
+++ WordCounter/Models/BackEnd.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace WordInASentence
+{
+  public class RepeatCounters
+  {
+    public static Dictionary<int,string> WordsContainer {get; set;}
+    public int  GrandTotal {get; set;}
+    public bool  WordIsThere {get; set;}
+    public RepeatCounters()
+    {
+      WordsContainer = new Dictionary <int,string> { {1,""}, {2,""}, {3,""}, {4, ""},{5, ""},{6,""},{7,""} };
+      GrandTotal =0;
+      WordIsThere = false;
+
+    }
+    
+    public void DoesItContain (string Word, string Sentence)
+    {
+      
+    }
+
+    public void NumberOfTimes (string Word, string Sentence)
+    {
+      
+    }
+  }
+}
