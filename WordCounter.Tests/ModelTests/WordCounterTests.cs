a86c477a7d64a0e2cc25c4fb6e64a9df0e1c5658
nkhakwan <post_khan@yahoo.com>
2020-05-15 10:15:18 -0700

    committing test code outlining how test would be done on the code to be written

---
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shapes.Models;
+
+
+namespace WordInASentence
+{
+  [TestClass]
+  public class RepeatCounterTests
+  {
+    [TestMethod]
+    public void DoesItContain_ChecksIfWordIsContained_WordIsThereUpdated()
+    {
+      // Arrange
+      
+
+      // Act
+      
+
+      // Assert
+      
+
+    }
+
+    [TestMethod]
+    public void NumberOfTimes_ChecksWhetherCountedWordCorrect_GrandTotalIsUpdated()
+    {
+      // Arrange
+      
+
+      // Act
+      
+
+      // Assert
+      
+
+    }
+  }
+}
