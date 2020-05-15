0e3e01891402e3f6778288ba7f0033efc57cb278
nkhakwan <post_khan@yahoo.com>
2020-05-15 14:05:44 -0700

    committing two more tests. Additional test are failing as expected values are wrong

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -39,5 +39,38 @@ namespace WordInASentence
       Assert.AreEqual(2, MyRepeatCounter.GrandTotal);
 
     }
+
+    [TestMethod]
+    public void DoesItContain_ChecksIfWordIsContained_ExpectingZero()
+    {
+      // Arrange
+      string Word = "cat";
+      string Sentence = "It is a dog sentence and reverse of tac is not here";
+
+      // Act
+      RepeatCounters MyRepeatCounter = new RepeatCounters();
+      MyRepeatCounter.DoesItContain(Word, Sentence);
+
+      // Assert
+      Assert.AreEqual(1,MyRepeatCounter.WordIsThere);
+
+    }
+
+    [TestMethod]
+    public void NumberOfTimes_ChecksWhetherCountedWordCorrect_GrandTotalEqualsZero()
+    {
+      // Arrange
+      string Word = "cat";
+      string Sentence = "My cats live in Petcat village with other cats that look more like a dog";
+
+
+      // Act
+       RepeatCounters MyRepeatCounter = new RepeatCounters();
+       MyRepeatCounter.NumberOfTimes(Word, Sentence);
+
+      // Assert
+      Assert.AreEqual(2, MyRepeatCounter.GrandTotal);
+
+    }
   }
 }
